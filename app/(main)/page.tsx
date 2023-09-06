f9b42fdf1de74821e968d569225a4bcf6674bf23
Berat Hazer <berathazer371@gmail.com>
2023-09-06 14:09:00 +0300

    project setup

---
+++ app/(main)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+export default function Home() {
+	return (
+		<main id="homeMain" className="p-20 ">
+			<p>Hello discord-clone</p>
+			<Button variant={"outline"}>Deneme Butonu</Button>
+		</main>
+	);
+}
